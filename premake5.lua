bcdcb075ecaba829e76096faf37bf84b916acddb
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2021-04-24 17:47:52 -0300

    Setup premake file to automate generation of the projects.

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Stein"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Stein"
+    location "Stein"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "ST_PLATFORM_WINDOWS",
+            "ST_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+    filter "configurations:Debug"
+        defines "ST_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ST_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ST_DIST"
+        optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Stein/vendor/spdlog/include",
+        "Stein/src"
+    }
+
+    links 
+    {
+        "Stein"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "ST_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "ST_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ST_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "ST_DIST"
+        optimize "On"
